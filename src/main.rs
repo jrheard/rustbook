1d2e08df7811ba048cabef70c544ad3b3600a0ff
jrheard <jrheard@cs.stanford.edu>
2019-01-27 15:55:07 -0800

    split mean/median/mode out into their own functions

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,21 @@ use std::collections::HashMap;
 
 use colored::*;
 
-// Returns the mean, median, and mode of `nums`.
-fn stats(nums: &Vec<i32>) -> (f64, f64, f64){
-
-    // XXX - is this shadowing+copying technique idiomatic or terrible?
-    let mut nums = nums.to_vec();
-
+fn mean(nums: &Vec<i32>) -> f64 {
     // Mean.
     let mut mean = 0.0;
 
-    for num in &nums {
+    for num in nums {
         mean += *num as f64;
     }
 
     mean /= nums.len() as f64;
 
-    // Median.
-    let median;
+    mean
+}
+
+fn median(nums: &Vec<i32>) -> f64 {
+    let mut nums = nums.to_vec();
 
     nums.sort();
 
@@ -28,17 +26,18 @@ fn stats(nums: &Vec<i32>) -> (f64, f64, f64){
         let before_midpoint = nums[(nums.len() / 2) - 1] as f64;
         let after_midpoint = nums[nums.len() / 2] as f64;
 
-        median = (before_midpoint + after_midpoint) / 2.0;
+        (before_midpoint + after_midpoint) / 2.0
     } else {
-        median = nums[nums.len() / 2] as f64;
+        nums[nums.len() / 2] as f64
     }
+}
 
-    // Mode.
+fn mode(nums: &Vec<i32>) -> i32{
     // If there are multiple modes, I just pick one arbitrarily. In reality, it looks like
     // it's possible for a set of numbers to have zero, one, or several modes.
     let mut occurrences = HashMap::new();
 
-    for num in &nums {
+    for num in nums {
         let count = occurrences.entry(*num).or_insert(0);
         *count += 1;
     }
@@ -53,8 +52,7 @@ fn stats(nums: &Vec<i32>) -> (f64, f64, f64){
         }
     }
 
-    // All done.
-    (mean, median, mode.into())
+    mode
 }
 
 fn main() {
@@ -67,9 +65,7 @@ fn main() {
 
     let x = vec![1, 2, 3, 4, 5, 10, 200, 3, 2, 4];
 
-    let (mean, median, mode) = stats(&x);
-
-    println!("The mean, median, and mode of {:?} are {}, {}, and {}.", &x, mean, median, mode);
+    println!("The mean, median, and mode of {:?} are {}, {}, and {}.", &x, mean(&x), median(&x), mode(&x));
 
     println!("\n{}", "Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!".green());
     println!("{}", "UNIMPLEMENTED".red());
