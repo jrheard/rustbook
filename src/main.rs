56803c0884ef440b565cedd0162e98aaf3eb09dd
jrheard <jrheard@cs.stanford.edu>
2019-01-26 19:39:23 -0800

    tweak

--- src/main.rs
+++ src/main.rs
@@ -58,14 +58,22 @@ fn stats(nums: &Vec<i32>) -> (f64, f64, f64){
 }
 
 fn main() {
-    println!("\n\n{}\n", "Implementations of suggested exercises from the Rust book.".white().bold());
+    println!("\n\n{}\n\n", "Implementations of suggested exercises from the Rust book.".white().bold());
+
 
     println!("{}", "Chapter 8:".green().bold());
-    println!("{}", "Given a list of integers, use a vector and return the mean, median, and mode of the list.".green());
+
+    println!("\n{}", "Given a list of integers, use a vector and return the mean, median, and mode of the list.".green());
 
     let x = vec![1, 2, 3, 4, 5, 10, 200, 3, 2, 4];
 
     let (mean, median, mode) = stats(&x);
 
-    println!("The mean, median, and mode of {:?} are {}, {}, and {}.", &x, mean, median, mode)
+    println!("The mean, median, and mode of {:?} are {}, {}, and {}.", &x, mean, median, mode);
+
+    println!("\n{}", "Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!".green());
+    println!("{}", "UNIMPLEMENTED".red());
+
+    println!("\n{}", "Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.".green());
+    println!("{}", "UNIMPLEMENTED".red());
 }
