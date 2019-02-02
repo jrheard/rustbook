25bc8235e2bc5a65f762dfb0905f42b52437b125
jrheard <jrheard@cs.stanford.edu>
2019-02-02 12:15:14 -0800

    convert chapter 8 exercises to tests

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@
 
 use std::collections::HashMap;
 
-use colored::*;
+use assert_approx_eq::assert_approx_eq;
 
 
 fn mean(nums: &Vec<i32>) -> f64 {
+    if nums.len() == 0 {
+        return 0.0;
+    }
+
     let mut mean = 0.0;
 
     for num in nums {
@@ -18,6 +22,10 @@ fn mean(nums: &Vec<i32>) -> f64 {
 }
 
 fn median(nums: &Vec<i32>) -> f64 {
+    if nums.len() == 0 {
+        return 0.0;
+    }
+
     let mut nums = nums.to_vec();
 
     nums.sort();
@@ -83,56 +91,88 @@ fn pig_latin(a_str: &str) -> String {
     latinized_words.join(" ")
 }
 
+// TODO lifetimes
 
 // Pretty sure &strs would be more natural than Strings, but I haven't learned about lifetimes yet.
-fn add_employee(directory: &mut HashMap<String, Vec<String>>, command: &str) -> () {
+fn add_employee(directory: &mut HashMap<String, Vec<String>>, command: &str) {
     // Parse the command.
     let words: Vec<&str> = command.split(" ").collect();
     let employee = words[1];
     let department = words[3];
 
-    let department_entry = directory.entry(department.to_string()).or_insert(Vec::new() as Vec<String>);
+    let department_entry = directory.entry(department.to_string())
+        .or_insert(Vec::new() as Vec<String>);
     department_entry.push(employee.to_string());
 }
 
-fn print_employees(directory: &HashMap<String, Vec<String>>) -> () {
+fn sort_employees(directory: &HashMap<String, Vec<String>>) -> Vec<String> {
     let mut keys: Vec<&String> = directory.keys().collect();
     keys.sort();
+
+    let mut result: Vec<String> = Vec::new();
 
     for department in &keys {
         let mut employees = directory.get(*department).unwrap().clone();
         employees.sort();
-        println!("{} has employees {:?}", department, employees);
+        result.push(format!("{} has employees {:?}", department, employees));
     }
 
+    result
 }
 
-fn main() {
-    println!("\n\n{}\n\n", "Implementations of suggested exercises from the Rust book.".bold());
+#[cfg(test)]
+mod chapter_8 {
+    use super::*;
 
+    #[test]
+    fn test_mean() {
+        assert_approx_eq!(mean(&vec![10,2,38,23,38,23,21]), 22.142857);
+        assert_eq!(mean(&vec![4, 8, 15, 16, 23, 42]), 18.0);
+        assert_eq!(mean(&vec![5]), 5.0);
+        assert_eq!(mean(&vec![]), 0.0);
+    }
 
-    println!("{}", "Chapter 8:".green().bold());
+    #[test]
+    fn test_median() {
+        assert_eq!(median(&vec![1, 2, 3, 4, 5, 10, 200, 3, 2, 4]), 3.5);
+        assert_eq!(median(&vec![10,2,38,23,38,23,21]), 23.0);
+        assert_eq!(median(&vec![5]), 5.0);
+        assert_eq!(median(&vec![]), 0.0);
+    }
 
-    println!("\n{}", "Given a list of integers, use a vector and return the mean, median, and mode of the list.".green());
+    #[test]
+    fn test_mode() {
+        assert!([2, 3, 4].contains(
+                &mode(&vec![1, 2, 3, 4, 5, 10, 200, 3, 2, 4])));
+        assert_eq!(mode(&vec![23,10,2,38,23,38,23,21]), 23);
+        assert_eq!(mode(&vec![5]), 5);
+        assert_eq!(mode(&vec![]), 0);
+    }
 
-    let x = vec![1, 2, 3, 4, 5, 10, 200, 3, 2, 4];
+    #[test]
+    fn test_pig_latin() {
+        assert_eq!(pig_latin("Hello there how are you I am fine"), "ello-Hay here-tay ow-hay are-hay ou-yay I-hay am-hay ine-fay");
+    }
 
-    // TODO - change all these to tests / assertions?
+    #[test]
+    fn test_employee_directory() {
+        let mut directory = HashMap::new();
+        add_employee(&mut directory, "Add Sally to Engineering");
+        add_employee(&mut directory, "Add Amir to Sales");
+        add_employee(&mut directory, "Add Bob to Engineering");
+        add_employee(&mut directory, "Add Cathy to Engineering");
+        add_employee(&mut directory, "Add Joseph to Sales");
+        add_employee(&mut directory, "Add Angela to Finance");
 
-    println!("The mean, median, and mode of {:?} are {}, {}, and {}.", &x, mean(&x), median(&x), mode(&x));
+        let result = sort_employees(&directory);
 
-    println!("\n{}", "Convert strings to pig latin.".green());
-    println!("{}", pig_latin("Hello there how are you I am fine"));
+        // TODO rustfmt
+        assert_eq!(result,
+                   vec![
+                   "Engineering has employees [\"Bob\", \"Cathy\", \"Sally\"]",
+                   "Finance has employees [\"Angela\"]",
+                   "Sales has employees [\"Amir\", \"Joseph\"]"])
 
-    println!("\n{}", "Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.".green());
+    }
 
-    let mut directory = HashMap::new();
-    add_employee(&mut directory, "Add Sally to Engineering");
-    add_employee(&mut directory, "Add Amir to Sales");
-    add_employee(&mut directory, "Add Bob to Engineering");
-    add_employee(&mut directory, "Add Cathy to Engineering");
-    add_employee(&mut directory, "Add Joseph to Sales");
-    add_employee(&mut directory, "Add Angela to Finance");
-
-    print_employees(&directory);
 }
