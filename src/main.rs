6db7f0b39e873c0ac859894e17572552ee3e8bc1
jrheard <jrheard@cs.stanford.edu>
2019-02-02 12:16:40 -0800

    run rustfmt for my first ever time

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::collections::HashMap;
 
 use assert_approx_eq::assert_approx_eq;
 
-
 fn mean(nums: &Vec<i32>) -> f64 {
     if nums.len() == 0 {
         return 0.0;
@@ -40,7 +39,7 @@ fn median(nums: &Vec<i32>) -> f64 {
     }
 }
 
-fn mode(nums: &Vec<i32>) -> i32{
+fn mode(nums: &Vec<i32>) -> i32 {
     // If there are multiple modes, I just pick one arbitrarily. In reality, it looks like
     // it's possible for a set of numbers to have zero, one, or several modes.
     let mut occurrences = HashMap::new();
@@ -72,7 +71,6 @@ fn mode(nums: &Vec<i32>) -> i32{
 // instead (“apple” becomes “apple-hay”).
 // (Intentionally not handling punctuation.)
 fn pig_latinize_word(word: &str) -> String {
-
     // (This code taken wholesale from Peter Huene, because I was having a lot of trouble
     // figuring out a sane way of expressing it because I'm still learning. Thanks, Peter!
     let first = word.chars().nth(0).unwrap();
@@ -85,7 +83,8 @@ fn pig_latinize_word(word: &str) -> String {
 fn pig_latin(a_str: &str) -> String {
     // I don't know what a Rust closure is just yet, but I really wanted to use the map operation
     // here so I'm using it before I've gotten to that part of the book. This is fine.
-    let latinized_words: Vec<String> = a_str.split(" ")
+    let latinized_words: Vec<String> = a_str
+        .split(" ")
         .map(|word| pig_latinize_word(word))
         .collect();
     latinized_words.join(" ")
@@ -100,7 +99,8 @@ fn add_employee(directory: &mut HashMap<String, Vec<String>>, command: &str) {
     let employee = words[1];
     let department = words[3];
 
-    let department_entry = directory.entry(department.to_string())
+    let department_entry = directory
+        .entry(department.to_string())
         .or_insert(Vec::new() as Vec<String>);
     department_entry.push(employee.to_string());
 }
@@ -126,7 +126,7 @@ mod chapter_8 {
 
     #[test]
     fn test_mean() {
-        assert_approx_eq!(mean(&vec![10,2,38,23,38,23,21]), 22.142857);
+        assert_approx_eq!(mean(&vec![10, 2, 38, 23, 38, 23, 21]), 22.142857);
         assert_eq!(mean(&vec![4, 8, 15, 16, 23, 42]), 18.0);
         assert_eq!(mean(&vec![5]), 5.0);
         assert_eq!(mean(&vec![]), 0.0);
@@ -135,23 +135,25 @@ mod chapter_8 {
     #[test]
     fn test_median() {
         assert_eq!(median(&vec![1, 2, 3, 4, 5, 10, 200, 3, 2, 4]), 3.5);
-        assert_eq!(median(&vec![10,2,38,23,38,23,21]), 23.0);
+        assert_eq!(median(&vec![10, 2, 38, 23, 38, 23, 21]), 23.0);
         assert_eq!(median(&vec![5]), 5.0);
         assert_eq!(median(&vec![]), 0.0);
     }
 
     #[test]
     fn test_mode() {
-        assert!([2, 3, 4].contains(
-                &mode(&vec![1, 2, 3, 4, 5, 10, 200, 3, 2, 4])));
-        assert_eq!(mode(&vec![23,10,2,38,23,38,23,21]), 23);
+        assert!([2, 3, 4].contains(&mode(&vec![1, 2, 3, 4, 5, 10, 200, 3, 2, 4])));
+        assert_eq!(mode(&vec![23, 10, 2, 38, 23, 38, 23, 21]), 23);
         assert_eq!(mode(&vec![5]), 5);
         assert_eq!(mode(&vec![]), 0);
     }
 
     #[test]
     fn test_pig_latin() {
-        assert_eq!(pig_latin("Hello there how are you I am fine"), "ello-Hay here-tay ow-hay are-hay ou-yay I-hay am-hay ine-fay");
+        assert_eq!(
+            pig_latin("Hello there how are you I am fine"),
+            "ello-Hay here-tay ow-hay are-hay ou-yay I-hay am-hay ine-fay"
+        );
     }
 
     #[test]
@@ -166,13 +168,14 @@ mod chapter_8 {
 
         let result = sort_employees(&directory);
 
-        // TODO rustfmt
-        assert_eq!(result,
-                   vec![
-                   "Engineering has employees [\"Bob\", \"Cathy\", \"Sally\"]",
-                   "Finance has employees [\"Angela\"]",
-                   "Sales has employees [\"Amir\", \"Joseph\"]"])
-
+        assert_eq!(
+            result,
+            vec![
+                "Engineering has employees [\"Bob\", \"Cathy\", \"Sally\"]",
+                "Finance has employees [\"Angela\"]",
+                "Sales has employees [\"Amir\", \"Joseph\"]"
+            ]
+        )
     }
 
 }
